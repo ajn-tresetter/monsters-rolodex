5c23bd26e065179e0371888a9de241367ab92b68
@adam.nenning <adam.nenning@tresetter.com>
2020-09-30 22:22:10 -0500

    Updates

---
+++ precache-manifest.af3e57c70439f640c30d12c60d52eb56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c271f604fa3e0eebe60e14c83ea27c1c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8fbb1c2967083da8770b",
+    "url": "/monsters-rolodex/static/css/main.4bac7aa1.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbb1c2967083da8770b",
+    "url": "/monsters-rolodex/static/js/main.dad48184.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
